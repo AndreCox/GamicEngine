94fc9a06faa71190e6f9ac4e877e5b45cdb5344b
Acropolis40 <arc.xoc@gmail.com>
2021-01-05 05:47:41 -0800

    Weird Bug in sound loading through media handler causes a missing dll file error which makes no sense as we're using static librarys :(

--- Game Engine/MediaHandler.cpp
+++ Game Engine/MediaHandler.cpp
@@ -32,7 +32,7 @@ sf::Font MediaHandler::loadfont(std::string path)
 		spdlog::warn("Could not load font! From " + path + "!");
 	}
 }
-
+/*
 sf::SoundBuffer MediaHandler::loadsound(std::string path)
 {
 	if (path.substr(path.find_last_of(".") + 1) == "wav" || path.substr(path.find_last_of(".") + 1) == "ogg") //checks if file is a image file!
@@ -47,3 +47,34 @@ sf::SoundBuffer MediaHandler::loadsound(std::string path)
 		spdlog::warn("Could not load sound! From " + path + "!");
 	}
 }
+*/
+
+/*
+file type number codes
+
+0 = textures
+1 = fonts
+2 = sounds
+
+*/
+
+void MediaHandler::pulldata(std::string path, int type) //Function called to load in a specific media type using functions declared above.
+{
+	switch (type)
+	{
+	case 0:
+		spdlog::info("Texure type detected.");
+		loadtexture(path);	
+		break;
+	case 1:
+		spdlog::info("Font type detected.");
+		loadfont(path);
+		break;
+	case 2:
+		spdlog::info("Sound type detected.");
+		//loadsound(path);
+		break;
+	}
+	
+}
+
--- Game Engine/MediaHandler.h
+++ Game Engine/MediaHandler.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <string>
 #include "SFML/Graphics.hpp"
-#include "SFML/Audio.hpp"
+//#include "SFML/Audio.hpp"
 class MediaHandler
 {
 public:
 	sf::Texture loadtexture(std::string path); //function called to load textures.
 	sf::Font loadfont(std::string path); //function called to load fonts
-	sf::SoundBuffer loadsound(std::string path); //function called to load sound
-	//void pulldata(std::string path);
+	//sf::SoundBuffer loadsound(std::string path); //function called to load sound
+	void pulldata(std::string path, int type);
 };
 
 
